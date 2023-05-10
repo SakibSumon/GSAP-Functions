f2b23e3f4a4408f7451b1aeb47764058a8eb04cb
Sakib Rahman Sumon <42873074+SakibSumon@users.noreply.github.com>
2023-05-10 18:33:27 +0600

    Update gsap-functions.js

--- gsap-functions.js
+++ gsap-functions.js
@@ -1,5 +1,5 @@
  ScrollTrigger.batch(".slider-anim", {
     start: "top 90%",
     onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: .12, duration: .3}),
-    console.log('github gsap triggered')
+   
 })
